ccb2648a46e87ef2657ade711ed8852dc4f346a0
Tra <kodaimaa@gmail.com>
2019-06-12 11:09:07 +0700

    upload image

---
+++ app/src/main/java/com/example/dev20_2/model/Notification.java
@@ -0,0 +1,70 @@
+package com.example.dev20_2.model;
+
+public class Notification {
+    private String email;
+    private String lat;
+    private String lng;
+    private int type;
+    private int upvote;
+    private int downvote;
+
+    public Notification() {
+    }
+
+    public Notification(String email, String lat, String lng, int type, int upvote, int downvote) {
+        this.email = email;
+        this.lat = lat;
+        this.lng = lng;
+        this.type = type;
+        this.upvote = upvote;
+        this.downvote = downvote;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getUpvote() {
+        return upvote;
+    }
+
+    public void setUpvote(int upvote) {
+        this.upvote = upvote;
+    }
+
+    public int getDownvote() {
+        return downvote;
+    }
+
+    public void setDownvote(int downvote) {
+        this.downvote = downvote;
+    }
+}
